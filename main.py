f9e8d98bb79f7c0ba24f75a0646a766b603ff97d
denis <Den_1994_2015@mail.ru>
2024-12-06 11:11:59 +0300

    create new functions

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # my practice OOP
 
+from httpx import post
+
+
 class Person:
     """класс для создания персоны 'человек', также для изменения и хранения информации о конкретном человеке"""
     def __init__(self, name=None, lastname=None, age=None, gender=None, kids=None, job=None, adress=None, 
@@ -14,14 +17,14 @@ class Person:
         self.profession = profession
 
 
-    # функция для возврата информации о человеке
+    # function for return person's info
     def get_info(self):
         info = {'gender': self.gender, 'Name': self.name, 'lastname': self.last_name, 'Age': self.age, 
                 'Kids': self.kids, 'job': self.job, 'adress': self.adress, 'profession': self.profession}
         return info
     
 
-    # функция для добавления или изменения имени человека
+    # function for change or add person's name
     def set_name(self, name):
         if type(name) is str:
             self.name = name
@@ -29,7 +32,7 @@ class Person:
             raise ValueError('Введены неккоректные данные')
 
 
-    # функция для добавления или изменения  фамилии человека
+    # function for change or add person's last name
     def set_last_name(self, last_name):
         if type(last_name) is str:  
             self.last_name = last_name
@@ -37,7 +40,7 @@ class Person:
             raise ValueError('Введены неккоректные данные')
 
 
-    # функция для добавления или изменения фамилии человека
+    # fuvction for change or add person's age
     def set_age(self, age):
         if type(age) is int:
             self.age = age
@@ -45,29 +48,29 @@ class Person:
             raise ValueError('Введены неккоректные данные')
 
 
-    # функция для добавления детей
+    # function for add person's kids
     def add_chield(self, chield):
         if self.kids is None:
             self.kids = []
         self.kids.append(chield)
     
 
-    # функция для добавления или изменения адреса
+    # function for chenge or add person's adress
     def set_adress(self, adress):
         self.adress = adress
 
 
-    # функция для добавления или изменения места работы человека
+    # function for change or add person's job
     def set_job(self, job):
         self.job = job
 
 
-    # функция для добавления или изменения профессии человека
+    # function for change or add person's prfession
     def set_profession(self, profession):
         self.profession = profession
     
 
-    # функция для установки или изменения пола человека
+    # function for change or set person's gender
     def set_gender(self, gender):
         if gender.lower() in ['мужской', 'женский', 'male', 'female']:
             self.gender = gender
@@ -76,34 +79,47 @@ class Person:
 
     
  
-# создаем класс работник(Worcker) и наследуемся от класса персона(Person)
+# create class Woecker and inherited from class Person
 class Worcker(Person):
     """класс для представления, создания или изменения информации о работнике"""
     def __init__(self, post=None, salery=None, expirience=None, level=None):
-        super.__init__(name=None, lastname=None, age=None, gender=None, kids=None, job=None, adress=None, 
+        super().__init__(name=None, lastname=None, age=None, gender=None, kids=None, job=None, adress=None, 
                         profession=None)
         self.post = post
         self.salery = salery
         self.expirience = expirience
         self.level = level
 
-
+    # function for return information about worcker
     def show_info(self):
-        pass
+        info = {'name': self.name, 'lastname': self.last_name, 'age': self.age, 'gender': self.gender, 
+                'kids': self.kids, 'job': self.job, 'adress': self.adress, 'profession': self.profession, 
+                'post': self.post, 'salery': self.salery, 'expirience': self.expirience, 'level': self.level}
+        return info
 
 
-    # функция для изменения или добавления должности сотрудника
+    # function for change or add worcker's post
     def set_post(self, post):
         self.post = post
 
-
+    # function for change or add worcker's salery
     def set_salery(self, salery):
-        pass
+        if type(salery) is int or type(salery) is float:
+            self.salery = salery
+        else:
+            raise ValueError('ВВедите число')
 
-
+    # function for change or add worcker's expirience
     def set_expirience(self, expirience):
-        pass
+        self.expirience = expirience
 
-
+    # function for change or add worckwe's level
     def set_level(self, level):
-        pass
+        self.level = level
+
+
+
+
+per = Worcker('denis', 'Glone', '25', 'male')
+
+print(per.show_info())
